74325e99940ca6624407aed231ed39c204b3c33d
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-11 13:43:37 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,60 @@
+'''name:goutham
+date:11-05-2022
+title:datatypes
+'''
+#integer part the data type is int
+
+# a=6;b=5;c=8
+# print(a)
+# print(b)
+# print(c)
+# print('%d'%a) 
+# print('%i'%b)
+# print('%d'%c)
+# #type( ) will give the what kind of datatype it is
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# '''%d or %i is a format specifier gor integers'''
+
+#float part: the datetype is float
+# a=5.5
+# b=3.2
+# c=5.8
+# print('%f'%a)
+# print('%.2f'%b)
+# print('%.5f'%c)
+# print('%.3f'%a)
+# print('%1f'%c)
+# print('%2f'%c)
+# print('%3f'%c)
+# print('%4f'%c) 
+# print('%5f'%c)
+# print('%6f'%c)
+# print('%7f'%c)
+# print('%8f'%c)
+# print('%9f'%c) 
+# length of c is 8 and given formate is 9%f so, (9-8=1) it will give one space before printing value of c:_5.800000
+# print('%10f'%c)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+'''string type'''
+# a='hi';b='hello';c='bye'
+# print('%s'%a)
+# print('%s'%b)
+# print('%s'%c)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print('%.2s'%c)
+
+'''boolean : boolean will gives the true or false'''
+# print(6>7)
+# print(9>8)
+# print(6==7)
+# print(6!=7)
+
+
+
